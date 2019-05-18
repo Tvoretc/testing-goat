cbbfadbf226e0996d44e974ede30d163aa160d40
Tvoretc <rus.tvoretc@gmail.com>
2019-05-18 20:40:16 +0300

    Added saving of the items

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,18 +8,37 @@ from lists.models import Item
 
 class IndexPageTest(TestCase):
 
-    def test_index_url_resolves_to_index_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, indexView)
-
     def test_index_view_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/index.html')
 
-    def test_post(self):
-        response = self.client.post('/', data={'item_text' : 'New item'})
-        self.assertIn('New item', response.content.decode())
-        self.assertTemplateUsed(response, 'lists/index.html')
+    def test_post_can_save(self):
+        test_item_text = 'New item'
+        response = self.client.post('/', data={'item_text' : test_item_text})
+        first_item = Item.objects.first()
+
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(first_item.text, test_item_text)
+
+    def test_redirect_after_post(self):
+        response = self.client.post('/', data={'item_text' : 'test'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_save_items_when_nessesary(self):
+        count = Item.objects.count()
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), count)
+
+    def test_displays_all_list(self):
+        Item.objects.create(text = 'item 1')
+        Item.objects.create(text = 'item 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from lists.models import Item
 # Create your views here.
 
 def indexView(request):
-    return render(request, 'lists/index.html', {'new_item_text' : request.POST.get('item_text', '')})
+    print('entered index view. Method = ' + request.method)
+    if request.method == 'POST':
+        Item.objects.create(text = request.POST['item_text'])
+        return redirect('/')
+
+    return render(request, 'lists/index.html', {
+        'items' : Item.objects.all()
+    })
