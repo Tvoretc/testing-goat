bbe7eeb39bf973f85eb2de78ab6850e020479a8b
Tvoretc <rus.tvoretc@gmail.com>
2019-06-08 14:57:50 +0300

    added bootstrap to static and centered layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome('C:\\webdrivers\\chromedriver.exe')
@@ -91,7 +91,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('test')
         inputbox.send_keys(Keys.ENTER)
-        
+
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
