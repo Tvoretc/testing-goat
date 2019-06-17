c303371bec64f170035e742c381fe0cb87adc1da
Tvoretc <rus.tvoretc@gmail.com>
2019-06-17 17:01:33 +0300

    fixed hardcoded urls

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -3,6 +3,6 @@ from lists import views
 
 urlpatterns = [
     path('new', views.newListView, name = 'new_list'),
-    path('<int:list_id>/', views.listView, name = 'view_list'),
+    path('<int:list_id>/', views.listView, name = 'list_view'),
     # path('<int:list_id>/add_item', views.newItemView, name = 'new_item'),
 ]
