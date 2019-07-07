1f3a95c688baad4e5274b28ec83e64acdfb2c9f1
unknown <rus.tvoretc@gmail.com>
2019-07-07 19:54:44 +0300

    added qunit tests, jquery

--- superlists/lists/static/tests/tests.js
+++ superlists/lists/static/tests/tests.js
@@ -1,5 +1,19 @@
-QUnit.test('smoke test', function (assert){
-  assert.equal($('.has-error').is(':visible'), true, 'visible');
-  $('.has-error').hide();
-  assert.equal($('.has-error').is('visible'), false, 'hidden');
+// QUnit.test('smoke test', function (assert){
+//   assert.equal($('.has-error').is(':visible'), true, 'visible');
+//   $('.has-error').hide();
+//   assert.equal($('.has-error').is('visible'), false, 'hidden');
+// });
+
+var initialize = function () {
+  $('input[name="text"]').on('keypress', function(){
+    $('.has-error').hide();});
+};
+
+QUnit.test('errors are not hidden if there is no keypress', function(assert){
+  assert.equal($('.has-error').is(':visible'), true);
+});
+
+QUnit.test('errors should be hidden on keypress', function (assert){
+  $('input[name="text"]').trigger('keypress');
+  assert.equal($('.has-error').is(':visible'), false);
 });
