fd8936d7b6c3b390c8ac9bd57789433b6ebc8376
Dmytro Tvoretc <rus.tvoretc@gmail.com>
2019-07-12 20:50:19 +0300

    FT for login via email

---
+++ superlists/functional_tests/test_login.py
@@ -0,0 +1,32 @@
+from django.core import mail
+from selenium.webdriver.common.keys import Keys
+import re
+
+from .base import FunctionalTest
+
+TEST_EMAIL = 'test@mail.com'
+SUBJECT = 'Your login link for Superlists'
+
+class LoginTest(FunctionalTest):
+
+    def test_can_get_email_link_to_login(self):
+        self.browser.get(self.live_server_url)
+        input = self.browser.find_element_by_name('email')
+        input.send_keys(TEST_EMAIL)
+        input.send_keys(Keys.ENTER)
+
+        self.assertIn('Check your email', self.browser.find_element_by_tag_name('body').text)
+        email = mail.outbox[0]
+        self.assertIn(TEST_EMAIL, email.to)
+        self.assertEqual(email.subject, SUBJECT)
+
+        self.assertIn('Use this link to log in', email.body)
+        url_search = re.search(r'http://.+/.+$', email.body)
+        if not url_search:
+            self.fail('Could not find url in email body')
+        url = url_search.group(0)
+        self.assertIn(self.live_server_url, url)
+        self.browser.get(url)
+        self.browser.find_element_by_link_text('Log out')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(TEST_EMAIL, navbar.text)
